207057f4dfe8200ac60e95be252c363a28e6fcb4
Higor <higordpb@gmail.com>
2024-05-06 16:36:01 -0300

    Atualização 06/05 - Consegui fazer rodar e fiz o getAll de denuncias

--- AtivoOperante2024/src/main/java/br/unoeste/fipp/ativooperante2024/db/repositories/DenunciaRepository.java
+++ AtivoOperante2024/src/main/java/br/unoeste/fipp/ativooperante2024/db/repositories/DenunciaRepository.java
@@ -1,10 +1,12 @@
 package br.unoeste.fipp.ativooperante2024.db.repositories;
 
 import br.unoeste.fipp.ativooperante2024.db.entities.Denuncia;
-import br.unoeste.fipp.ativooperante2024.db.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
+
+    Object getAll();
 }
--- AtivoOperante2024/src/main/java/br/unoeste/fipp/ativooperante2024/services/DenunciaService.java
+++ AtivoOperante2024/src/main/java/br/unoeste/fipp/ativooperante2024/services/DenunciaService.java
@@ -3,14 +3,24 @@ package br.unoeste.fipp.ativooperante2024.services;
 import br.unoeste.fipp.ativooperante2024.db.entities.Denuncia;
 import br.unoeste.fipp.ativooperante2024.db.repositories.DenunciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DenunciaService {
 
     @Autowired
     private DenunciaRepository denrepo;
 
-    public Denuncia getDenuncia (Long id)
+    public Optional<Denuncia> getDenuncia (Long id)
     {
         return denrepo.findById(id);
+    }
+
+    public List<Denuncia> getAllDenuncias()
+    {
+        return denrepo.findAll();
     }
 }
